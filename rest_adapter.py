233ac0d28d496aeb474507d6facf836cb6939589
Nicholas Vanhaute <nvanhaute@wisc.edu>
2024-06-19 12:06:17 -0500

    Refactor GET and POST methods

--- rest_adapter.py
+++ rest_adapter.py
@@ -11,11 +11,16 @@ class RestAdapter:
             requests.packages.urllib3.disable_warnings()
 
     def get(self, endpoint: str, ep_params: Dict=None) -> List[Dict]:
+        return self._do(http_method="GET", endpoint=endpoint, ep_params=ep_params)
+    
+    def post(self, endpoint: str, ep_params: Dict=None, data: Dict=None):
+        return self._do(http_method="POST", endpoint=endpoint, ep_params=ep_params, data=data)
+    
+    def _do(self, http_method: str, endpoint: str, ep_params: Dict=None, data: Dict=None):
         full_url = self.url + endpoint
         headers = {"Authorization": f"APIKey {self._api_key}"}
-        response = requests.get(url=full_url, verify=self._ssl_verify, headers=headers, params=ep_params)
+        response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify, headers=headers, params=ep_params, json=data)
         data_out = response.json()
         if response.status_code >= 200 and response.status_code <= 299:
-            return data_out
+            return None
         raise Exception(data_out["errors"])
-    
