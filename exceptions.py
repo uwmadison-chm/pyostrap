5fd5212d47bf3eaa504106e28dbaba3378e345bd
Nicholas Vanhaute <nvanhaute@wisc.edu>
2024-06-19 12:10:54 -0500

    Add basic exception handling

---
+++ exceptions.py
@@ -0,0 +1,2 @@
+class BiostrapApiException(Exception):
+    pass
--- rest_adapter.py
+++ rest_adapter.py
@@ -2,6 +2,8 @@ import requests
 import requests.packages
 from typing import List, Dict
 
+from .exceptions import BiostrapApiException
+
 class RestAdapter:
     def __init__(self, hostname: str, api_key: str='', ver: str='v1', ssl_verify: bool=True):
         self.url = f"https://{hostname}/{ver}/"
@@ -10,17 +12,24 @@ class RestAdapter:
         if not ssl_verify:
             requests.packages.urllib3.disable_warnings()
 
+    def _do(self, http_method: str, endpoint: str, ep_params: Dict=None, data: Dict=None):
+        full_url = self.url + endpoint
+        headers = {"Authorization": f"APIKey {self._api_key}"}
+        try:
+            response = requests.request(method=http_method, url=full_url,
+                                        verify=self._ssl_verify, headers=headers,
+                                        params=ep_params, json=data)
+        except requests.exceptions.RequestException as e:
+            raise BiostrapApiException("Request failed") from e
+        
+        data_out = response.json()
+        if response.status_code >= 200 and response.status_code <= 299:
+            return None
+        raise Exception(data_out["errors"])
+    
     def get(self, endpoint: str, ep_params: Dict=None) -> List[Dict]:
         return self._do(http_method="GET", endpoint=endpoint, ep_params=ep_params)
     
     def post(self, endpoint: str, ep_params: Dict=None, data: Dict=None):
         return self._do(http_method="POST", endpoint=endpoint, ep_params=ep_params, data=data)
     
-    def _do(self, http_method: str, endpoint: str, ep_params: Dict=None, data: Dict=None):
-        full_url = self.url + endpoint
-        headers = {"Authorization": f"APIKey {self._api_key}"}
-        response = requests.request(method=http_method, url=full_url, verify=self._ssl_verify, headers=headers, params=ep_params, json=data)
-        data_out = response.json()
-        if response.status_code >= 200 and response.status_code <= 299:
-            return None
-        raise Exception(data_out["errors"])
