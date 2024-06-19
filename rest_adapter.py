9e9c2fa8d1b1a6a72394159bf44ed203c86da472
Nicholas Vanhaute <nvanhaute@wisc.edu>
2024-06-19 12:29:03 -0500

    Add logging

--- rest_adapter.py
+++ rest_adapter.py
@@ -1,4 +1,5 @@
 from json import JSONDecodeError
+import logging
 from typing import Dict
 
 import requests
@@ -10,11 +11,12 @@ from .models import Result
 
 class RestAdapter:
     def __init__(
-        self, hostname: str, api_key: str = "", ver: str = "v1", ssl_verify: bool = True
+        self, hostname: str, api_key: str = "", ver: str = "v1", ssl_verify: bool = True, logger: logging.Logger = None
     ):
         self.url = f"https://{hostname}/{ver}/"
         self._api_key = api_key
         self._ssl_verify = ssl_verify
+        self._logger = logger or logging.getLogger(__name__)
         if not ssl_verify:
             requests.packages.urllib3.disable_warnings()
 
@@ -23,7 +25,10 @@ class RestAdapter:
     ) -> Result:
         full_url = self.url + endpoint
         headers = {"Authorization": f"APIKey {self._api_key}"}
+        log_line_pre = f"method={http_method}, url={full_url}, params={ep_params}"
+        log_line_post = ", ".join((log_line_pre, "success={}, status_code={}, message={}"))
         try:
+            self._logger.debug(msg=log_line_pre)
             response = requests.request(
                 method=http_method,
                 url=full_url,
@@ -33,15 +38,22 @@ class RestAdapter:
                 json=data,
             )
         except requests.exceptions.RequestException as e:
+            self._logger.error(msg=(str(e)))
             raise BiostrapApiException("Request failed") from e
 
         try:
             data_out = response.json()
         except (ValueError, JSONDecodeError) as e:
+            self._logger.error(msg=log_line_post.format(False, None, e))
             raise BiostrapApiException("Bad JSON in response") from e
 
-        if 200 <= response.status_code <= 299:
+        is_success = 200 <= response.status_code <= 299
+        log_line = log_line_post.format(is_success, response.status_code, response.reason)
+        
+        if is_success:
+            self._logger.debug(msg=log_line)
             return Result(response.status_code, message=response.reason, data=data_out)
+        self._logger.error(msg=log_line)
         raise BiostrapApiException(f"{response.status_code}: {response.reason}")
 
     def get(self, endpoint: str, ep_params: Dict = None) -> Result:
